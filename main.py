c31e83bcbe2857b23a5c8042819e1f11a8e0443c
Роман Чуклинов <xjt@mail.ru>
2022-11-22 01:48:16 +1000

    слияние с кодом из kaggle

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import email
 import json
 import logging
 import os
 import smtplib
 import ssl
 import sys
-import time
 from datetime import datetime
 from email import encoders
 from email.mime.base import MIMEBase
@@ -13,14 +11,15 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import requests
 import seaborn as sns
 import urllib3
 from dotenv import load_dotenv
-from pandas.io.json import json_normalize
 from IPython.display import display
+from pandas.io.json import json_normalize
+
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -49,7 +48,6 @@ MAIL_TO = os.getenv('MAIL_TO')
 
 auth_url = "https://www.strava.com/api/v3/oauth/token"
 activites_url = "https://www.strava.com/api/v3/athlete/activities"
-# PROXIES = {'http': "socks5://72.195.34.59:4145", 'https': "socks5://72.195.34.59:4145"}
 
 
 def get_tokens(refresh_token):
@@ -146,14 +144,103 @@ def main():
         for item in my_dataset:
             if str(item['map']['summary_polyline']) != 'None':
                 result += "'" + str(item['map']['summary_polyline']) + "',\n"
-        
+
         with open('tracks.json', 'w', encoding='utf-8') as f:
             f.write(result)
 
-        
     except Exception as e:
         logger.error(f'Функция: {__name__}. Ошибка: {e}')
 
 
-if __name__ == '__main__':
-    main()
+# -------------------------------------------------------------------------
+
+pd.options.display.float_format = '{:.1f}'.format
+DISPL_COUNT = 5
+
+with open('response.json', 'r', encoding='utf-8') as f:
+    my_dataset = json.load(f)
+
+df = pd.json_normalize(my_dataset)
+
+df = df[df['type'] == 'Ride']
+
+df['start_date_local'] = pd.to_datetime(df['start_date_local'])
+df['start_time'] = df['start_date_local'].dt.time
+df = df.assign(avg_move_spd_kmh=df['average_speed'] * 3.6)
+df = df.assign(avg_total_spd_kmh=df['distance'] / df['elapsed_time'] * 3.6)
+df = df.assign(avg_elev_perc=df['total_elevation_gain'] / df['distance'] * 100)
+df = df.assign(dist_km=df['distance'] / 1000)
+df = df.assign(start_date_r=df.start_date_local.dt.strftime('%d.%m.%Y'))
+df = df.assign(start_my=df.start_date_local.dt.strftime('%Y.%m'))
+df = df.assign(start_time_r=df.start_date_local.dt.strftime('%H:%M'))
+df = df.assign(month=df.start_date_local.dt.month)
+df = df.assign(weekday=df.start_date_local.dt.weekday)
+df = df.fillna(0)
+df['gear_id'] = df['gear_id'].replace(to_replace='b7149139', value='Corratec Corones')
+df['gear_id'] = df['gear_id'].replace(to_replace='b6130097', value='GT Agressor')
+df['gear_id'] = df['gear_id'].replace(to_replace='b8541718', value='Nishiki')
+df['gear_id'] = df['gear_id'].replace(to_replace='b6856164', value='ХВЗ')
+
+# ------------------------------- вывод данных ------------------------------
+
+# display(pd.pivot_table(df, index=['id']))
+
+print(f"Пробег на велосипеде всего: {df['dist_km'].sum().round(1)} км")
+
+print("")
+
+print(f"Средняя скорость (только в движении): {df['avg_move_spd_kmh'].mean().round(1)} км/ч")
+print("...в том числе по велосипедам:")
+display(df.groupby('gear_id').agg({'avg_move_spd_kmh':'mean', 'dist_km':'sum'})\
+      .sort_values(by='avg_move_spd_kmh', axis=0, ascending=False)\
+      .rename(columns={'gear_id':'Велосипед', 'avg_move_spd_kmh':'V движ.ср., км/ч','dist_km':'Общий пробег, км'}))
+
+print("")
+
+print(f"Средняя скорость (с учетом стоянок): {df['avg_total_spd_kmh'].mean().round(1)} км/ч")
+print("...в том числе по велосипедам:")
+display(df.groupby('gear_id').agg({'avg_total_spd_kmh':'mean', 'dist_km':'sum'})\
+      .sort_values(by='avg_total_spd_kmh', axis=0, ascending=False)\
+      .rename(columns={'gear_id':'Велосипед', 'avg_total_spd_kmh':'V полн. ср., км/ч','dist_km':'Общий пробег, км'}))
+
+print("")
+
+print(f"Средний набор высоты: {df['avg_elev_perc'].mean().round(2)} %")
+print("...в том числе по велосипедам:")
+display(df.groupby(['gear_id'])[['gear_id', 'avg_elev_perc']].mean()\
+      .sort_values(by='avg_elev_perc', axis=0, ascending=False)\
+      .rename(columns={'gear_id':'Велосипед', 'avg_elev_perc':'Средн. набор, %'})\
+      .style.format(precision=2))
+
+print("")
+
+print(f"Кол-во поездок по дням недели:")
+display(df.groupby(['weekday']).agg({'weekday':'count', 'dist_km':'sum'})\
+      .rename(columns={'weekday':'Кол-во заездов', 'dist_km':'Дистанция, км'})\
+      .style.format(precision=1).background_gradient(cmap='Blues'))
+
+print("")
+
+print(f"Пробег по месяцам года:")
+df_moy = df.groupby(['month']).agg({'month':'count', 'dist_km':'sum'})\
+        .rename({'month':'Месяц', 'dist_km':'дистанция, км'})\
+        
+display(df_moy.style.format(precision=1).background_gradient(cmap='Blues'))
+
+df_moy[['month', 'dist_km']].plot(kind="pie", label="", subplots=True)
+
+print("")
+
+print(f"Показатели за все время, по месяцам:")
+display(df.groupby(['start_my']).agg({'dist_km':'sum', 'avg_move_spd_kmh':'mean', 'avg_total_spd_kmh':'mean', 'avg_elev_perc':'mean'})\
+        .rename(columns={'start_my':'Месяц', 'dist_km':'Дистанция, км', 'avg_move_spd_kmh':'Vср.движ., км/ч', 'avg_total_spd_kmh':'Vср.полн., км/ч', 'avg_elev_perc':'Средн. набор, %'})\
+        .style.format(precision=1).background_gradient(cmap='Blues'))
+              
+print("")
+
+print(f"Последние {DISPL_COUNT} тренировок:")
+display(df[['name', 'start_date_r', 'start_time_r', 'dist_km', 'avg_move_spd_kmh', 'avg_total_spd_kmh', 'avg_elev_perc']]\
+          .head(DISPL_COUNT)\
+          .rename(columns={'name':'Название', 'start_date_r':'Дата','start_time_r':'Время','dist_km':'Дистанция, км',\
+                 'avg_move_spd_kmh':'V движ.ср., км/ч','avg_total_spd_kmh':'V полн.ср., км/ч','avg_elev_perc':'Средн. набор, %'})\
+          .style.format(precision=1))
